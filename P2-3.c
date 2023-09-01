2331cf83afce0d0a093c129ddfc827b61b90c3c2
Emanuel Gioda <ema.gioda1@gmail.com>
2023-09-01 18:53:08 -0300

    Practica 2 - Ejercicio 3

---
+++ P2-3.c
@@ -0,0 +1,23 @@
+//3- Dado un número introducido por el usuario, indicar si este es positivo, negativo o nulo.
+
+#include <stdio.h>
+
+int main(){
+    float num;
+
+    printf("Ingrese un numero: \n");
+    scanf("%f", &num);
+
+    if (num > 0)
+    {
+        printf("%.2f es positivo\n", num);
+    }
+    else if (num < 0)
+    {
+        printf("%.2f es negativo\n", num);
+    }
+    else {
+        printf("El numero es igual a 0\n");
+    }
+    return 0;
+}
