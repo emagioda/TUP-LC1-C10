019adc015cc9b2942223b66a03062973ef3fd42c
Emanuel Gioda <ema.gioda1@gmail.com>
2023-09-02 10:07:45 -0300

    Practica 2 - Ejercicio 5

---
+++ P2-5.c
@@ -0,0 +1,18 @@
+//5- Escriba un programa que pida ingresar un número y a continuación escriba en la consola si el mismo es par o impar.
+
+#include <stdio.h> 
+
+int main(){
+    int num;
+
+    printf("Ingrese un numero: \n");
+    scanf("%i", &num);
+    
+    if (num % 2 == 0){
+        printf("Es par.\n");
+    }
+    else{
+        printf("Es impar.\n");
+    }
+    return 0;
+}
