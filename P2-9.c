9220f02774403d33c947680d8e35fe80b2350865
Emanuel Gioda <ema.gioda1@gmail.com>
2023-09-06 20:44:52 -0300

    Practica 2 - Ejercicio 9

---
+++ P2-9.c
@@ -0,0 +1,26 @@
+// Escribir un algoritmo que pida la altura de una persona, si la altura es menor o igual a 150 cm envíe el mensaje: “Persona de altura por debajo de la media”; si la altura está entre 151 y 170 escriba el mensaje: “Persona de altura media” y si la altura es mayor al 170 escriba el mensaje: “Persona de altura por arriba de la media”
+
+#include <stdio.h>
+
+int main(){
+
+    float altura;
+
+    printf("Ingrese su altura:\n");
+    scanf("%f", &altura);
+
+    if (altura <= 0){
+        printf("Altura Invalida\n");
+    }
+    else if (altura <= 150){
+        printf("Persona de altura por debajo de la media\n");
+    }
+    else if(altura < 170){
+        printf("Persona de altura media\n");
+    }
+    else{
+        printf("Persona de altura por arriba de la media\n");
+    }
+
+    return 0;
+}
