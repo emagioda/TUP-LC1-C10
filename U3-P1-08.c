b22b1c5e8959021bd2a6773c7ac78c80049fc40d
Emanuel Gioda <ema.gioda1@gmail.com>
2023-09-09 00:24:21 -0300

    Unidad 3 - Practica 1 - Ejercicio 8

---
+++ U3-P1-08.c
@@ -0,0 +1,23 @@
+// Actualizar el saldo de una cuenta bancaria, a partir de un retiro. 
+// Verificar que el saldo sea mayor o igual que el importe a retirar.
+
+#include <stdio.h>
+
+int main(){
+    int saldo, retiro;
+    
+    saldo = 100000;
+
+    printf("Ingrese el monto a reitirar: \n");
+    scanf("%i", &retiro);
+
+    if (saldo >= retiro){
+        saldo -= retiro;
+        printf("El saldo actual de la cuenta es de $%i.\n", saldo);
+    }
+    else{
+        printf("No se puede realizar la operacion\n");
+    }
+
+    return 0;
+}
