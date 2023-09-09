401bb61d43f1f897ec9cb9509441176d50496939
Emanuel Gioda <ema.gioda1@gmail.com>
2023-09-09 00:23:39 -0300

    Unidad 3 - Practica 1 - Ejercicio 6

---
+++ U3-P1-06.c
@@ -0,0 +1,30 @@
+/*
+Leer una nota de un alumno por consola e informar la condición del mismo. 
+(menor que 4: Reprobado, menor que 6: Regular, mayor o igual que 6: Promocionado). 
+Validar que la nota ingresada se encuentre entre el 0 y el 10, de lo contrario informar “Nota inválida”.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int nota;
+
+    printf("Ingrese la nota del alumno: \n");
+    scanf("%i", &nota);
+
+    if (nota < 0 || nota > 10){
+        printf("Nota Invalida.\n");
+    }
+    else if (nota < 4){
+        printf("Reprobado\n");
+    }
+    else if (nota < 6){
+        printf("Regular\n");
+    }
+    else {
+        printf("Aprobado\n");
+    }
+    
+    return 0;
+}
