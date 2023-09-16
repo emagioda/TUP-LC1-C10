67a5755eba0968d22c6f96138dce87f641f66d40
Emanuel Gioda <ema.gioda1@gmail.com>
2023-09-16 10:56:57 -0300

    Unidad 3 - Practica 3 - Ejercicio 10

---
+++ U3-P3-10.c
@@ -0,0 +1,25 @@
+/*
+Imprimir por consola 10 veces el carácter @, de manera que se visualice la siguiente
+salida:
+@
+@
+@
+@
+@
+@
+@
+@
+@
+@
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("@\n");
+    }
+    return 0;
+}
