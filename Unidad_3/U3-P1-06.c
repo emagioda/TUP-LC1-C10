a0205187b3654a50c7db0e54107e61cab1746cc7
Emanuel Gioda <142058958+emagioda@users.noreply.github.com>
2023-10-11 20:22:22 -0300

    Unidad 3 - Practica 1 - Ejercicio 6

--- Unidad_3/U3-P1-06.c
+++ Unidad_3/U3-P1-06.c
@@ -1,7 +1,7 @@
 /*
 Leer una nota de un alumno por consola e informar la condición del mismo. 
 (menor que 4: Reprobado, menor que 6: Regular, mayor o igual que 6: Promocionado). 
-Validar que la nota ingresada se encuentre entre el 0 y el 10, de lo contrario informar “Nota inválida”.
+Validar que la nota ingresada se encuentre entre el 0 y el 10, de lo contrario informar “Nota inválida”
 */
 
 #include <stdio.h>
