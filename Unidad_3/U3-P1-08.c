c329397cb9b0b28b7b1bf2e1239de30a14192d4b
Emanuel Gioda <142058958+emagioda@users.noreply.github.com>
2023-10-11 20:23:28 -0300

    Unidad 3 - Practica 1 - Ejercicio 8

--- Unidad_3/U3-P1-08.c
+++ Unidad_3/U3-P1-08.c
@@ -1,5 +1,7 @@
-// Actualizar el saldo de una cuenta bancaria, a partir de un retiro. 
-// Verificar que el saldo sea mayor o igual que el importe a retirar.
+/*
+Actualizar el saldo de una cuenta bancaria, a partir de un retiro. 
+Verificar que el saldo sea mayor o igual que el importe a retirar.
+*/
 
 #include <stdio.h>
 
