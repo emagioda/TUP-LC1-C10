de4dc8eacf267bb4976c1ed2aea92651f0785e46
Emanuel Gioda <142058958+emagioda@users.noreply.github.com>
2023-10-11 20:43:25 -0300

    Unidad 3 - Practica 4 - Ejercicio 3

--- Unidad_3/U3-P4-03.c
+++ Unidad_3/U3-P4-03.c
@@ -1,8 +1,8 @@
 /*
 Pedir al usuario que ingrese 10 números reales, validar que los mismos sean valores
 positivos, si el usuario ingresa un número negativo o nulo, pedir reingresar el número
-(repetir el ingreso de dicho número mientras el número no sea correcto). Informar al
-usuario la suma de los 10 números ingresados.
+(repetir el ingreso de dicho número mientras el número no sea correcto). 
+Informar al usuario la suma de los 10 números ingresados.
 */
 
 #include <stdio.h>
