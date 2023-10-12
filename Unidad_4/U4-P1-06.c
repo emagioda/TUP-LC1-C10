44f22409e6fa548013c8e14cbb4efe80b7f17d90
Emanuel Gioda <142058958+emagioda@users.noreply.github.com>
2023-10-11 21:40:33 -0300

    Unidad 4 - Practica 1 - Ejercicio 6

--- Unidad_4/U4-P1-06.c
+++ Unidad_4/U4-P1-06.c
@@ -1,9 +1,9 @@
 /*
 Ingrese por teclado la facturación de los últimos 6 meses. Informar:
-    a. La facturación total
-    b. El promedio de facturación
-    c. La máxima facturación
-    d. La mínima facturación 
+    a. La facturación total.
+    b. El promedio de facturación.
+    c. La máxima facturación.
+    d. La mínima facturación.
 */
 
 #include <stdio.h>
