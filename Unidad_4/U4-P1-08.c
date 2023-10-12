844ed47059e5d41960e432ba47677163974d2f9b
Emanuel Gioda <142058958+emagioda@users.noreply.github.com>
2023-10-11 21:43:19 -0300

    Unidad 4 - Practica 1 - Ejercicio 8

--- Unidad_4/U4-P1-08.c
+++ Unidad_4/U4-P1-08.c
@@ -1,6 +1,6 @@
 /*
-Leer 10 números por teclado, 5 para un array y 5 para otro array distinto. Mostrar los
-10 números en pantalla mediante un solo array.
+Leer 10 números por teclado, 5 para un array y 5 para otro array distinto. 
+Mostrar los 10 números en pantalla mediante un solo array.
 */
 
 #include <stdio.h>
