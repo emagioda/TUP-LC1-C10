70a9bd1f672eb945d32db6ebc6e1bf03bee2552b
Emanuel Gioda <142058958+emagioda@users.noreply.github.com>
2023-10-11 21:39:00 -0300

    Update U4-P1-03.c

--- Unidad_4/U4-P1-03.c
+++ Unidad_4/U4-P1-03.c
@@ -1,6 +1,6 @@
 /*
 Declarar un array de tipo entero de 5 componentes, asignarles un valor por consola,
-mostrar cuántas veces se repite el número 5.
+mostrar cuántas veces se repite el número 5
 */
 #include <stdio.h>
 
