d54038aa63758fb3a742b15d72bd9c69c4db631e
Emanuel Gioda <142058958+emagioda@users.noreply.github.com>
2023-10-11 21:43:47 -0300

    Unidad 4 - Practica 1 - Ejercicio 9

--- Unidad_4/U4-P1-09.c
+++ Unidad_4/U4-P1-09.c
@@ -1,6 +1,6 @@
 /*
 Escriba un programa que lea 5 números por teclado, los copie a otro array
-multiplicados por 2 y muestre el segundo array
+multiplicados por 2 y muestre el segundo array.
 */
 
 #include <stdio.h>
